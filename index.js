355113b21e883b1faa7528f59e5af3c9545d80f1
AlderSan <gentr023@gmail.com>
2025-12-05 12:18:20 -0600

    Add Pseudocode

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const http = require('node:http');
 
 const server = http.createServer((req, res) => {
 //server code goes here
+//need to get URL and store as variable
+//need to verify if there is an error or unknown URL to return 404.html
+//check URL to set webpages
+//blank will load index.html
 
 });
 
