480ebe1933ddcc5394ec86548e43b9d43a2ebcbb
AlderSan <gentr023@gmail.com>
2025-12-05 18:40:58 -0600

    Implement Express for routing

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const fs = require('fs');
 const url = require('url');
 const http = require('node:http');
+const express = require('express');
+const app = express();
+const PORT = 8080;
 
-const server = http.createServer(function(req, res){
+app.get(["/", "/index", "/index.html"], (req, res) => res.write(fs.readFileSync('./index.html')));
+app.get(["/about", "/about.html"], (req, res) => res.write(fs.readFileSync('./about.html')));
+app.get(["/contact", "/contact.html"], (req, res) => res.write(fs.readFileSync('./contact.html')));
+app.get(/.*/, (req, res) => res.write(fs.readFileSync("./404.html")));
+
+
+//remaking with express
+app.listen(PORT, (error) => {
+    if (error) {
+        throw error;
+    }
+    console.log(`Express server started, listening on port ${PORT}`);
+})
+
+/* const server = http.createServer(function(req, res){
     let currentURL = url.parse(req.url);
     currentURL = currentURL.pathname === '/' ? currentURL.pathname = '/index.html' : currentURL;
     let fileName = '.' + currentURL.pathname;
@@ -21,4 +38,4 @@ const server = http.createServer(function(req, res){
 
 });
 
-server.listen(8080);
+server.listen(8080); */
